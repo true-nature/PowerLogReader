7facfee04f07d75c40e352c8f39ecaf9fcdd2172
Yasuhiro MAGARA <gama@users.osdn.me>
2021-01-06 21:31:09 +0900

    Prevent duplicate launch

--- PowerLogReader/App.xaml.cs
+++ PowerLogReader/App.xaml.cs
@@ -3,6 +3,9 @@ using PowerLogReader.Modules.Services;
 using PowerLogReader.Views;
 using Prism.Ioc;
 using Prism.Modularity;
+using System;
+using System.Reflection;
+using System.Threading;
 using System.Windows;
 
 namespace PowerLogReader
@@ -14,6 +17,11 @@ namespace PowerLogReader
     {
         protected override Window CreateShell()
         {
+            CreateLock();
+            if (LockObject == null)
+            {
+                return null;
+            }
             return Container.Resolve<MainWindow>();
         }
 
@@ -26,6 +34,36 @@ namespace PowerLogReader
         protected override void ConfigureModuleCatalog(IModuleCatalog moduleCatalog)
         {
             moduleCatalog.AddModule<PowerLogModule>();
+        }
+
+        private Mutex LockObject;
+
+        private void CreateLock()
+        {
+            var created = false;
+            try
+            {
+                var name = Assembly.GetEntryAssembly().GetName().Name;
+                LockObject = new Mutex(true, name, out created);
+            }
+            catch (Exception)
+            {
+                created = false;
+            }
+            if (!created)
+            {
+                LockObject?.Close();
+                LockObject = null;
+                Application.Current.Shutdown();
+            }
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            base.OnExit(e);
+            LockObject?.ReleaseMutex();
+            LockObject?.Close();
+            LockObject = null;
         }
     }
 }
